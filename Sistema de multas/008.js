a2ce7594d3af68493be83e1afd526b8ff15df36b
Thainá Nogueira <thainanog28@gmail.com>
2021-09-16 10:47:29 -0300

    Estudos diversos

---
+++ Sistema de multas/008.js
@@ -0,0 +1,6 @@
+var vel =5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60) {
+    console.log(`Você ultrapassou velocidade permitida.Multado!`)
+}
+console.log(`Dirigia sempre usando cinto de segurança!`)
